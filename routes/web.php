1eee27edfaa2edb9bdbaf00c62bbdaea677cf1d9
Erza Osmani <eerzaosmani@gmail.com>
2026-06-22 22:21:38 +0200

    Add models, controllers and resource routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\IssueController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\CommentController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('projects', ProjectController::class);
+Route::resource('issues', IssueController::class);
+Route::resource('tags', TagController::class);
+Route::resource('comments', CommentController::class);
